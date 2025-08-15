d45e356fe2c435a2de239532da8cc905a201edf0
Damjan Savic <info@damjan-savic.com>
2025-08-15 17:31:19 +0200

    Implementierung - SEO - 15.08.2025

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-be0f3d84'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.n4fh6kq3f8"
+    "revision": "0.pvpogjsn31o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
